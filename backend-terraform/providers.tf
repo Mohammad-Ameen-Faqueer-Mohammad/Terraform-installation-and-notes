18c6a495bfae9e0b2f3666774a02e8c1af63f11a
Mohammad Ameen <117063224+Mohammad-Ameen-Faqueer-Mohammad@users.noreply.github.com>
2025-10-01 13:57:14 +0530

    Create providers.tf

---
+++ backend-terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.14.1"
+    }
+  }
+
+#  backend "s3" {
+#    bucket         = "amin-ucket-from-tf"
+#    key            = "terraform.tfstate"
+#    region         = "ap-south-1"
+#    dynamodb_table = "my-dynamodb-table"
+#  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
